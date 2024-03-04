f05cc6827f9d72e602339cd2792214cb00e015ef
dagaither <gaither@protonmail.com>
2024-03-03 20:13:40 -0500

    Game complete + edge + validation

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const [hidden, setHidden] = useState("")
   const [guessedLetters, setGuessedLetters] = useState([])
   const [inputValue, setInputValue] = useState("")
+  const [guesses, setGuesses] =  useState(0)
 
   useEffect(() => randomWord(), [])
 
@@ -20,24 +21,56 @@ function App() {
   }
 
   const handleGuessedLetter = (e) => {
-    e.preventDefault()
-    let letter = e.target.letter.value
-    let newWord = ""
-    for (let i=0; i <puzzle.length; i++) {
-      if (letter === puzzle[i]) {
-        newWord += puzzle[i]
-      } 
-      else if (hidden[i] !== "_") {
-        newWord += hidden[i]
+    e.preventDefault();
+    let letter = inputValue.toLowerCase();
+  
+    if (guessedLetters.includes(letter)) {
+      alert("Already guessed!");
+      setInputValue("");
+      return;
+    }
+
+    if (letter.trim() == "") {
+      alert("Please enter a letter!");
+      setInputValue("");
+      return;
+    }
+
+    let lowerCasePuzzle = puzzle.toLowerCase();
+  
+    if (lowerCasePuzzle.includes(letter)) {
+      let newWord = "";
+      for (let i = 0; i < lowerCasePuzzle.length; i++) {
+        if (letter === lowerCasePuzzle[i]) {
+          newWord += puzzle[i];
+        } else if (hidden[i] !== "_") {
+          newWord += hidden[i];
+        } else {
+          newWord += "_";
+        }
       }
-      else {
-        newWord += "_"
+      if (guesses <= puzzle.length -1) {
+        setHidden(newWord);
+        setGuesses(prevGuesses => prevGuesses + 1);
+        setGuessedLetters(prevGuessedLetters => [...prevGuessedLetters, letter]);
+        setInputValue("");
+      } else {
+        gameOver();
+      }
+      if (newWord === lowerCasePuzzle) {
+        alert(`You win! The word was ${puzzle.toUpperCase()}`);
+        location.reload();
+      }
+    } else {
+      if (guesses <= puzzle.length -1) {
+        setGuesses(prevGuesses => prevGuesses + 1);
+        setGuessedLetters(prevGuessedLetters => [...prevGuessedLetters, letter]);
+        setInputValue("");
+      } else {
+        gameOver();
       }
     }
-    setHidden(newWord)
-    setInputValue("")
-  }
-
+  };
 
   const hiddenWord = (puzzle) => {
     let hidden = ""
@@ -47,9 +80,28 @@ function App() {
     return hidden
   }
 
+  const remainingGuesses = () => {
+    let remaining = (puzzle.length + 1) - guesses;
+    return `Remaining guesses: ${remaining}`
+  };
+
+  const gameOver = () => {
+    alert("Game over!");
+    location.reload();
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleGuessedLetter(e);
+    } else if (e.target.value.length >= 1 && e.key !== 'Backspace') {
+      e.preventDefault();
+    }
+  };
+
+
   return (
     <>
-    <h1>Hangman</h1>
+    <h1>React Hangman</h1>
     <h1>{hidden}</h1>
     <form id="userGuess" onSubmit={handleGuessedLetter}>
       <input 
@@ -58,10 +110,14 @@ function App() {
       placeholder="Guess a letter"
       value={inputValue}
       onChange={(e) => setInputValue(e.target.value)}
-      // onChange={(e) => setLetter(e.target.value)}
+      onKeyDown={handleKeyDown}
       ></input>
       <button type="submit">Submit</button>
     </form> 
+    <div className="guessed">
+    <h1>{guessedLetters.join(" ")}</h1>
+    <h2>{remainingGuesses()}</h2>
+    </div>
     </>
   )
 }
